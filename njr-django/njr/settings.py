494ce51b3db70e00a77be78825e7d8d2e070c2b4
Christian Gram Kalhauge <christian@kalhauge.dk>
2018-09-10 12:45:55 +0200

    Fix db back to docker and removed last query from list

--- njr-django/njr/settings.py
+++ njr-django/njr/settings.py
@@ -74,27 +74,27 @@ WSGI_APPLICATION = 'njr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'USER': 'root',
-#         'PASSWORD' : 'mypassword',
-#         'NAME' : 'njr_v2',
-#         'HOST': 'db', # Or an IP Address that your DB is hosted on
-#         'PORT': '3306'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'user': 'root',
-            'password' : 'supersecretpassword',
-            'database' : 'njr_v2'      
-        },
+        'USER': 'root',
+        'PASSWORD' : 'mypassword',
+        'NAME' : 'njr_v2',
+        'HOST': 'db', # Or an IP Address that your DB is hosted on
+        'PORT': '3306'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'user': 'root',
+#             'password' : 'supersecretpassword',
+#             'database' : 'njr_v2'      
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
